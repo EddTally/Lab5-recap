32ed2b868a58d8dc2ff19cd4a8de34aba40814da
Edward Tallentire <P15230019@LEC-ADMIN.dmu.ac.uk>
2017-11-02 14:53:57 +0000

    Stage 3 half done

---
+++ js/createElement.js
@@ -0,0 +1,17 @@
+(function(){
+	
+	let getText = document.getElementById("get-text");
+	
+	
+	function doStuff(ev){
+	  let para = document.createElement("p");
+	  let text = document.createTextNode("This is a new paragraph.");
+	  para.appendChild(text);
+	  let element = document.getElementById("div1);
+
+
+
+
+
+
+})();
